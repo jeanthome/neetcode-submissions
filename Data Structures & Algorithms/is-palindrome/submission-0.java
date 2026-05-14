6b13e0f4414a658fe47bc68bba0bfcac730adf5f
Jean Thomé <jtc.jean@gmail.com>
2026-05-14 19:30:36 -0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0, right = s.length() - 1;
+        System.out.print(s);
+        while (left < right) {
+            if (!isAlphaNum(s.charAt(left))) {
+                left ++;
+                continue;
+            };
+            if (!isAlphaNum(s.charAt(right))) {
+                right--;
+                continue;
+            };
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+
+    private boolean isAlphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
+}
